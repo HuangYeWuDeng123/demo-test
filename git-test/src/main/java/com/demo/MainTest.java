0159ab4ac5ba05aa53ca6db7a7102355a10c967a
liangchao <1948155230@qq.com>
2021-11-06 22:06:27 +0800

    test main

---
+++ git-test/src/main/java/com/demo/MainTest.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+/**
+ * @Author apple
+ * @Date 2021/11/6 22:05
+ * @Description //TODO
+ **/
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
